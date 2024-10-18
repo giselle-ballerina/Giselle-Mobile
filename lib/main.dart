44b0f6706c573cd94f034bd98194ed7e0e6eb659
chathura-de-silva <ac.chathura@gmail.com>
2024-10-18 15:40:34 +0530

    Minor Improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/services.dart';
 import 'views/home_page.dart';
 
 void main() {
   runApp(const MyApp());
+  enableFullScreen();
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +20,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  HomePage(),
+      home: HomePage(),
     );
   }
+}
+
+/// Removes status bar and navigation bar boxes for fullscreen experience.
+void enableFullScreen() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      systemNavigationBarColor: Colors.transparent,
+    ),
+  );
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -15,7 +15,8 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
             child: TextField(
               onChanged: (value) => productController.updateSearchQuery(value),
               decoration: InputDecoration(
@@ -33,35 +34,48 @@ class HomePage extends StatelessWidget {
 
               var products = productController.filteredProducts;
 
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.8,
-                ),
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  var product = products[index];
-                  return GestureDetector(
-                    onTap: () {
-                      // Get.to(() => ProductPage(product: product));
-                    },
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Image.network(product.images[0].url,
-                                fit: BoxFit.cover),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(product.productName,
-                                textAlign: TextAlign.center),
-                          ),
-                        ],
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.8,
+                  ),
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    var product = products[index];
+                    return GestureDetector(
+                      onTap: () {
+                        // Get.to(() => ProductPage(product: product));
+                      },
+                      child: Card(
+                        color: Colors.purple[100],
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(product.images[0].url,
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(product.productName,
+                                      textAlign: TextAlign.center),
+                                  Text("Rs. ${product.price}",
+                                      textAlign: TextAlign.center),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               );
             }),
           ),
