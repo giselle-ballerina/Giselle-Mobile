da3bf477e27aea3699a36993be59e7cee3eaf92f
chathura-de-silva <ac.chathura@gmail.com>
2024-10-19 13:03:10 +0530

    Search bar improved when there is no results and now only searches upon submission

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import '../controllers/product_controller.dart';
 // import 'product_page.dart';
 
+// ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
   final ProductController productController = Get.put(ProductController());
 
@@ -10,7 +11,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Giselle'),
+        title: const Text('Giselle'),
       ),
       body: Column(
         children: [
@@ -18,8 +19,12 @@ class HomePage extends StatelessWidget {
             padding:
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
             child: TextField(
-              onChanged: (value) => productController.updateSearchQuery(value),
-              decoration: InputDecoration(
+              onChanged: (value) => value == ""
+                  ? (productController.updateSearchQuery(value))
+                  : null,
+              onSubmitted: (value) =>
+                  productController.updateSearchQuery(value),
+              decoration: const InputDecoration(
                 labelText: 'Search',
                 border: OutlineInputBorder(),
                 prefixIcon: Icon(Icons.search),
@@ -29,53 +34,57 @@ class HomePage extends StatelessWidget {
           Expanded(
             child: Obx(() {
               if (productController.isLoading.value) {
-                return Center(child: CircularProgressIndicator());
+                return const Center(child: CircularProgressIndicator());
               }
 
               var products = productController.filteredProducts;
 
               return Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                    var product = products[index];
-                    return GestureDetector(
-                      onTap: () {
-                        // Get.to(() => ProductPage(product: product));
-                      },
-                      child: Card(
-                        color: Colors.purple[100],
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.network(product.images[0].url,
-                                    fit: BoxFit.cover),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
+                child: products.isNotEmpty
+                    ? GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.8,
+                        ),
+                        itemCount: products.length,
+                        itemBuilder: (context, index) {
+                          var product = products[index];
+                          return GestureDetector(
+                            onTap: () {
+                              // Get.to(() => ProductPage(product: product));
+                            },
+                            child: Card(
+                              color: Colors.purple[100],
                               child: Column(
                                 children: [
-                                  Text(product.productName,
-                                      textAlign: TextAlign.center),
-                                  Text("Rs. ${product.price}",
-                                      textAlign: TextAlign.center),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Image.network(
+                                          product.images[0].url,
+                                          fit: BoxFit.cover),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        Text(product.productName,
+                                            textAlign: TextAlign.center),
+                                        Text("Rs. ${product.price}",
+                                            textAlign: TextAlign.center),
+                                      ],
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                          );
+                        },
+                      )
+                    : const Center(child: Text('Sorry, No products found!')),
               );
             }),
           ),
