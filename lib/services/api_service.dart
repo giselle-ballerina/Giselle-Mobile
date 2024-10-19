7291615210c122e3285bd4c78dab9fd0c6849b61
chathura-de-silva <ac.chathura@gmail.com>
2024-10-19 15:52:11 +0530

    Shop+Product shuffle on homepage

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,18 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http_client;
 import '../models/product.dart';
+import '../models/shop.dart';
 
 class ApiService {
-  static const String apiUrl = 'http://139.59.246.168:9091/items';
+  static const String apiUrl = 'http://139.59.246.168:9091';
 
   static Future<List<Product>> fetchProducts() async {
-    final response = await http_client.get(Uri.parse(apiUrl));
+    final response = await http_client.get(Uri.parse('$apiUrl/items'));
 
     if (response.statusCode == 200) {
       List<dynamic> data = json.decode(response.body);
       return data.map((json) => Product.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load products');
+    }
+  }
+
+  static Future<List<Shop>> fetchShops() async {
+    final response = await http_client.get(Uri.parse('$apiUrl/shops'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => Shop.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load shops');
     }
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../controllers/shop_controller.dart';
 import '../controllers/product_controller.dart';
-// import 'product_page.dart';
+import '../models/product.dart' as product_model;
+import '../models/shop.dart' as shop_model;
 
 // ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
   final ProductController productController = Get.put(ProductController());
+  final ShopController shopController = Get.put(ShopController());
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +23,15 @@ class HomePage extends StatelessWidget {
                 const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
             child: TextField(
               onChanged: (value) => value == ""
-                  ? (productController.updateSearchQuery(value))
+                  ? {
+                      (productController.updateSearchQuery(value)),
+                      (shopController.updateSearchQuery(value))
+                    }
                   : null,
-              onSubmitted: (value) =>
-                  productController.updateSearchQuery(value),
+              onSubmitted: (value) => {
+                (productController.updateSearchQuery(value)),
+                (shopController.updateSearchQuery(value))
+              },
               decoration: const InputDecoration(
                 labelText: 'Search',
                 border: OutlineInputBorder(),
@@ -33,55 +41,92 @@ class HomePage extends StatelessWidget {
           ),
           Expanded(
             child: Obx(() {
-              if (productController.isLoading.value) {
+              if (productController.isLoading.value ||
+                  shopController.isLoading.value) {
                 return const Center(child: CircularProgressIndicator());
               }
 
-              var products = productController.filteredProducts;
+              var combinedList = [
+                ...productController.filteredProducts,
+                ...shopController.filteredShops
+              ];
 
               return Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: products.isNotEmpty
+                child: combinedList.isNotEmpty
                     ? GridView.builder(
                         gridDelegate:
                             const SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2,
                           childAspectRatio: 0.8,
                         ),
-                        itemCount: products.length,
+                        itemCount: combinedList.length,
                         itemBuilder: (context, index) {
-                          var product = products[index];
-                          return GestureDetector(
-                            onTap: () {
-                              // Get.to(() => ProductPage(product: product));
-                            },
-                            child: Card(
-                              color: Colors.purple[100],
-                              child: Column(
-                                children: [
-                                  Expanded(
-                                    child: Padding(
+                          var item = combinedList[index];
+                          if (item is product_model.Product) {
+                            return GestureDetector(
+                              onTap: () {
+                                // Get.to(() => ProductPage(product: product));
+                              },
+                              child: Card(
+                                color: Colors.purple[100],
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Image.network(item.images[0].url,
+                                            fit: BoxFit.cover),
+                                      ),
+                                    ),
+                                    Padding(
                                       padding: const EdgeInsets.all(8.0),
-                                      child: Image.network(
-                                          product.images[0].url,
-                                          fit: BoxFit.cover),
+                                      child: Column(
+                                        children: [
+                                          Text(item.productName,
+                                              textAlign: TextAlign.center),
+                                          Text("Rs. ${item.price}",
+                                              textAlign: TextAlign.center),
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      children: [
-                                        Text(product.productName,
-                                            textAlign: TextAlign.center),
-                                        Text("Rs. ${product.price}",
-                                            textAlign: TextAlign.center),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
+                            );
+                          } else if (item is shop_model.Shop) {
+                            return GestureDetector(
+                              onTap: () {
+                                // Get.to(() => ShopPage(shop: shop));
+                              },
+                              child: Card(
+                                color: Colors.purple[100],
+                                child: Column(
+                                  children: [
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Image.network(item.logo,
+                                            fit: BoxFit.cover),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: [
+                                          Text(item.shopName,
+                                              textAlign: TextAlign.center),
+                                          Text(item.description,
+                                              textAlign: TextAlign.center),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }
+                          return null;
                         },
                       )
                     : const Center(child: Text('Sorry, No products found!')),
